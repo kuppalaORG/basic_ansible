8cb85afa96da52c948e3f6c8d493caad95dd605f
bharath <bharathkumarkuppala@gmail.com>
2025-03-29 17:48:13 +0530

    RUN VAULT

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i $1.codedeploywithbharath.tech, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1  main.yml vault_token=hvs.Fl74YCWFL26w5rBvYn8VkwQx
+ansible-playbook -i $1.codedeploywithbharath.tech, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1  main.yml -e vault_token=hvs.Fl74YCWFL26w5rBvYn8VkwQx
